c85119aa6eefa2ab72ef70419056458332f70d8b
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-14 09:53:50 -0300

    Update Home Stylization

--- rocketshoesApp/src/components/Header/styles.js
+++ rocketshoesApp/src/components/Header/styles.js
@@ -13,7 +13,7 @@ export const Container = styled.View`
   flex-direction: row;
   flex: 1;
   justify-content: space-between;
-  padding: 20px;
+  padding: 10px;
 `;
 
 export const Logo = styled.Image.attrs({
--- rocketshoesApp/src/pages/Home/index.js
+++ rocketshoesApp/src/pages/Home/index.js
@@ -1,8 +1,77 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList } from 'react-native';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText,
+} from './styles';
 
-export default function Home() {
-  return <View />;
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../services/api';
+
+export default class Home extends React.Component {
+  state = {
+    products: [],
+  };
+
+  componentDidMount() {
+    this.getProduts();
+  }
+
+  getProduts = async () => {
+    const response = await api.get('/products');
+
+    const data = response.data.map(product => ({
+      ...product,
+    }));
+    console.tron.log(data);
+
+    this.setState({ products: data });
+  };
+
+  renderProduct = ({ item }) => {
+    // const { amount } = this.props;
+
+    return (
+      <Product key={item.id}>
+        <ProductImage
+          source={{
+            uri: item.image,
+          }}
+        />
+        <ProductTitle>{item.title}</ProductTitle>
+        <ProductPrice>{item.price}</ProductPrice>
+        <AddButton>
+          <ProductAmount>
+            <Icon name="add-shopping-cart" color="#FFF" size={20} />
+            <ProductAmountText>{item.amount || 0}</ProductAmountText>
+          </ProductAmount>
+          <AddButtonText>ADICIONAR</AddButtonText>
+        </AddButton>
+      </Product>
+    );
+  };
+
+  render() {
+    const { products } = this.state;
+    return (
+      <Container>
+        <FlatList
+          horizontal
+          data={products}
+          extraData={this.props}
+          keyExtractor={item => String(item.id)}
+          renderItem={this.renderProduct}
+        />
+      </Container>
+    );
+  }
 }
